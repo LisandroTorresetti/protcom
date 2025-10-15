df71261e42398b71663bcf9705fecc13abd6d833
Lisandro Torresetti <lisandrotorresetti@gmail.com>
2025-10-14 23:35:11 -0300

    feat: define header file for cmdparser API

---
+++ trabajo_final/Drivers/API/Inc/API_cmdparser.h
@@ -0,0 +1,10 @@
+#ifndef API_INC_API_CMDPARSER_H_
+#define API_INC_API_CMDPARSER_H_
+
+#include <stdbool.h>
+
+bool cmdparser_init();
+
+void cmdparser_read_cmd();
+
+#endif /* API_INC_API_CMDPARSER_H_ */
